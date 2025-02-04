0459354c510ce14a9660d7df353db584341aeea5
700007609 <700007609@2260I-L29-028>
2025-02-04 14:12:44 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Topic_1___Printing_Text
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Andrej Trajkovic 
+
+            Console.WriteLine("Parkside");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("I will print ont the same line");
+
+            Console.WriteLine();
+
+            Console.Write("Hello, World!");
+            Console.Write("I will print ont the same line");
+
+
+            Console.WriteLine("Something");
+        }
+    }
+}
